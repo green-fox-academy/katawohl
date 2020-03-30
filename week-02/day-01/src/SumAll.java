60cf08a0d35fd3051ab8eb0c6f0d1861b49efbdb
Kata Wohlmuth <katawohl@gmail.com>
2020-03-30 20:57:16 +0200

    prettified SumAll

--- week-02/day-01/src/SumAll.java
+++ week-02/day-01/src/SumAll.java
@@ -4,15 +4,15 @@
 
 public class SumAll {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int[] numbers = {3, 4, 5, 6, 7};
+    int[] numbers = {3, 4, 5, 6, 7};
 
-        int sum = 0;
+    int sum = 0;
 
-        for (int num : numbers) {
-            sum += num;
-        }
-        System.out.println("The sum of array elements is: " + sum);
+    for (int num : numbers) {
+      sum += num;
     }
+    System.out.println("The sum of array elements is: " + sum);
+  }
 }
