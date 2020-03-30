a7f4b1e209915ede220cee4e2171aac69d7624d9
Kata Wohlmuth <katawohl@gmail.com>
2020-03-30 20:37:40 +0200

    prettified ChangElement

--- week-02/day-01/src/ChangeElement.java
+++ week-02/day-01/src/ChangeElement.java
@@ -4,37 +4,35 @@
 // - Print the fourth element
 
 public class ChangeElement {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
+    int numbers[] = {1, 2, 3, 8, 5, 6};
 
-        int numbers[] = {1, 2, 3, 8, 5, 6};
-
-        //printing the original elements in the array
-
+    // region printing the original elements in the array
        /*
         for (int j = 0; j < numbers.length ; j++) {
             System.out.println(numbers[j]);
         }
         System.out.println();
         */
+    //endregion
 
-        int n = 8;
-        int n2 = 4;
+    int n = 8;
+    int n2 = 4;
 
-        //in this loop, the commented lines are there to print the whole array
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == n) {
-                numbers[i] = n2;
-            }
-            //System.out.println(numbers[i]);
-        }
-        //System.out.println();
+    //in this loop, the commented lines are there to print the whole array
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] == n) {
+        numbers[i] = n2;
+      }
+    }
 
-        System.out.println(numbers[3]);
+    System.out.println(numbers[3]);
 
-        //just another way to print the elements(?) in an array
+    // region just another way to print the elements(?) in an array
        /* for (int i = 0; i < numbers.length; i++)
             System.out.println("Element at index " + i +
                     " : "+ numbers[i]);*/
-    }
+    //endregion
+  }
 }
