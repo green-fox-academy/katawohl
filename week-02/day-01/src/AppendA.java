18151a03caef9a341825f3b10d61fe0f994bfe00
Kata Wohlmuth <katawohl@gmail.com>
2020-03-30 20:33:45 +0200

    simplified and corrected AppendA

--- week-02/day-01/src/AppendA.java
+++ week-02/day-01/src/AppendA.java
@@ -7,14 +7,13 @@ import java.util.Arrays;
 public class AppendA {
     public static void main(String[] args) {
 
-
         String[] animals = {"koal", "pand", "zebr"};
 
         String character = "a";
 
         for (int i = 0; i < animals.length; i++) {
-            animals[i] = animals[i] + character;
-            System.out.println(Arrays.toString(animals));
+            animals[i] += character;
         }
+        System.out.println(Arrays.toString(animals));
     }
 }
