12c029e5c9440a52f2621d29fa92092451fcf092
Kata Wohlmuth <katawohl@gmail.com>
2020-03-30 20:11:58 +0200

    prettified Bmi

--- week-01/day-04/src/Bmi.java
+++ week-01/day-04/src/Bmi.java
@@ -3,10 +3,7 @@ public class Bmi {
         double massInKg = 81.2;
         double heightInM = 1.78;
 
-        // Print the Body mass index (BMI) based on these values
         double heightInMSq = Math.pow(heightInM, 2);
-
-        //System.out.println(heightInMSq);
 
         double bmi = (massInKg / heightInMSq);
 
