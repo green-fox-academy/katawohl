c1a07fb230e41aaff29d0b098a597dae59b8df4c
Kata Wohlmuth <katawohl@gmail.com>
2020-03-30 21:06:04 +0200

    prettified SumElements

--- week-02/day-01/src/SumElements.java
+++ week-02/day-01/src/SumElements.java
@@ -3,10 +3,10 @@
 // - Print the sum of the second and the third element
 
 public class SumElements {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int[] numbers = {54, 23, 66, 12};
+    int[] numbers = {54, 23, 66, 12};
 
-        System.out.println(numbers[1] + numbers[2]);
-    }
+    System.out.println(numbers[1] + numbers[2]);
+  }
 }
