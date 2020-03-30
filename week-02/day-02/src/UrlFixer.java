6a4ddb69f1f60d3a71b23b61c6a416dc750e1399
Kata Wohlmuth <katawohl@gmail.com>
2020-03-30 21:20:13 +0200

    prettified UrlFixer

--- week-02/day-02/src/UrlFixer.java
+++ week-02/day-02/src/UrlFixer.java
@@ -3,36 +3,42 @@
 // Try to solve it more than once using different String functions!
 
 public class UrlFixer {
-    public static void main(String... args) {
-        String url = "https//www.reddit.com/r/nevertellmethebots";
+  public static void main(String... args) {
+    String url = "https//www.reddit.com/r/nevertellmethebots";
 
-        //method 1 replace function
-        url = url.replace("bots", "odds");
+    //method 1 replace function
+    url = url.replace("bots", "odds");
 
-        url = url.replace("https//", "https://");
-
-
-        System.out.println(url);
-
-        //method 2 with StringBuffer
-        //https://www.geeksforgeeks.org/stringbuffer-class-in-java/
-        //StringBuffer is a peer class of String that provides much of the functionality of strings. String
-        // represents fixed-length, immutable character sequences while StringBuffer represents growable and writable
-        // character sequences.
-        //
-        //StringBuffer may have characters and substrings inserted in the middle or appended to the end. It will
-        // automatically grow to make room for such additions and often has more characters preallocated than are
-        // actually needed, to allow room for growth.
-
-        StringBuffer url2 = new StringBuffer("https//www.reddit.com/r/nevertellmethebots");
-
-        url2.insert(5, ":");
-
-        System.out.println(url2.length());
-
-        url2.replace(39, 43, "odds");
+    url = url.replace("https//", "https://");
 
 
-        System.out.println(url2);
-    }
+    System.out.println(url);
+
+    //method 2 with StringBuffer
+      //region comments
+    //https://www.geeksforgeeks.org/stringbuffer-class-in-java/
+    //StringBuffer is a peer class of String that provides much of the functionality of strings.
+      // String
+    // represents fixed-length, immutable character sequences while StringBuffer represents
+      // growable and writable
+    // character sequences.
+    //
+    //StringBuffer may have characters and substrings inserted in the middle or appended to the
+      // end. It will
+    // automatically grow to make room for such additions and often has more characters
+      // preallocated than are
+    // actually needed, to allow room for growth.
+      //endregion
+
+    StringBuffer url2 = new StringBuffer("https//www.reddit.com/r/nevertellmethebots");
+
+    url2.insert(5, ":");
+
+    System.out.println(url2.length());
+
+    url2.replace(39, 43, "odds");
+
+
+    System.out.println(url2);
+  }
 }
