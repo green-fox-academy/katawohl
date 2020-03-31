e295f63aa6931a71ecc09efcbfd51bb091a1d9ed
Kata Wohlmuth <katawohl@gmail.com>
2020-03-31 21:15:44 +0200

    added Armada based on CA

--- week-03/day-03/src/pirates/Armada.java
+++ week-03/day-03/src/pirates/Armada.java
@@ -10,10 +10,29 @@ Contains Ship-s as a list
     return true if this is the winner
 */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Armada {
-  List<Ship> ships;
+  List<Ship> ships = new ArrayList<>();
 
+  public boolean war(Armada enemy){
+    int indexOfThis = 0;
+    int indexOfEnemy = 0;
 
+    while (indexOfThis < ships.size() && indexOfEnemy < enemy.ships.size()){
+      boolean thisWins = ships.get(indexOfThis).battle(enemy.ships.get(indexOfEnemy));
+      if (thisWins){
+        indexOfEnemy++;
+      } else {
+        indexOfThis++;
+      }
+    }
+
+    return indexOfThis<indexOfEnemy;
+  }
+
+  public void addShip(Ship ship){
+    ships.add(ship);
+  }
 }
