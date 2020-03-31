e6646db1334b6dc46cab0a9c23770ce541189c7b
Kata Wohlmuth <katawohl@gmail.com>
2020-03-31 21:10:48 +0200

    updated BattleApp accirding to CA

--- week-03/day-03/src/pirates/BattleShip.java
+++ week-03/day-03/src/pirates/BattleShip.java
@@ -9,43 +9,18 @@ import java.util.List;
 public class BattleShip {
 
   public static void main(String[] args) {
-    Ship ship1 = new Ship("Rosita");
-    Ship ship2 = new Ship("Juanita");
+    Ship ship1 = new Ship();
+    ship1.fillShip();
 
-    Pirates captain1 = new Pirates(3);
-    Pirates captain2 = new Pirates(4);
+    Ship ship2 = new Ship();
+    ship2.fillShip();
 
-    ship1.fillShip(captain1);
-    ship2.fillShip(captain2);
-
-    List<Pirates> crew1 = new ArrayList<>();
-
-    crew1.add(new Pirates(1));
-    crew1.add(new Pirates(9));
-    crew1.add(new Pirates());
-    crew1.add(new Pirates(5));
-    crew1.add(new Pirates(3));
-
-    ship1.fillShip(crew1);
-
-    List<Pirates> crew2 = new ArrayList<>();
-    crew2.add(new Pirates(6));
-    crew2.add(new Pirates(2));
-    crew2.add(new Pirates(1));
-    crew2.add(new Pirates(4));
-    crew2.add(new Pirates());
-    crew2.add(new Pirates(8));
-
-    ship2.fillShip(crew2);
+    boolean firstWins = ship1.battle(ship2);
+    System.out.println((firstWins ? "First" : "Second") + " ship won the battle");
 
     ship1.displayShip();
 
     ship2.displayShip();
 
-    ship1.battle(ship2);
-
-    ship1.displayShip();
-
-    ship2.displayShip();
   }
 }
