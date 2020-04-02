754fe14890cd96cb2369e025b2cf6cb40b2e8570
Kata Wohlmuth <katawohl@gmail.com>
2020-04-02 11:05:07 +0200

    refactored test names

--- week-03/day-04/tests/sum/SumTest.java
+++ week-03/day-04/tests/sum/SumTest.java
@@ -9,20 +9,20 @@ public class SumTest {
   Sum summy = new Sum();
 
   @Test
-  public void gimmeSumManyNums() {
+  public void gimmeSum_ManyNumsInArray_ReturnsSum() {
     int[] inputNumbers = {4, 5, 2, 2, 44};
     assertEquals(57, summy.gimmeSum(inputNumbers));
   }
 
   @Test
-  public void gimmeSumOneNum() {
+  public void gimmeSum_OneNumInArray_ReturnsThatNum() {
     int[] inputNumbers = {2};
 
     assertEquals(2, summy.gimmeSum(inputNumbers));
   }
 
   @Test
-  public void gimmeSumNull() {
+  public void gimmeSum_NullArray_ReturnsZero() {
     int[] inputNumbers = null;
 
     assertEquals(0, summy.gimmeSum(inputNumbers));
