4c9a3cedb9e235cd1314e1fd513ea44b47969d2c
Kata Wohlmuth <katawohl@gmail.com>
2020-04-14 17:14:58 +0200

    added Exercise 4

---
+++ week-06/day-01/src/Exercise4.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+    //Write a Stream Expression to get the average value of the odd numbers from the following list:
+
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+    double average =
+        numbers.stream()
+            .filter(number -> number % 2 != 0)
+            .collect(Collectors.averagingInt(number -> number));
+
+    System.out.println(average);
+  }
+}
