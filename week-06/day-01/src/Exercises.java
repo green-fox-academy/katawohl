88f626593919b2bb048b7cad384c5e4d638de15e
Kata Wohlmuth <katawohl@gmail.com>
2020-04-14 17:01:40 +0200

    added third exercise

--- week-06/day-01/src/Exercises.java
+++ week-06/day-01/src/Exercises.java
@@ -20,6 +20,16 @@ public class Exercises {
         .filter(number -> number >= 0)
         .map(number -> number *= number)
         .forEach(System.out::println);
+
+    System.out.println("--------------------------");
+
+    //Write a Stream Expression to find which number squared value is more then 20 from the following list:
+
+    List<Integer> numbers2 = Arrays.asList(3, 9, 2, 8, 6, 5);
+
+    numbers2.stream()
+        .filter(number -> number*number > 20)
+        .forEach(System.out::println);
   }
 
 }
