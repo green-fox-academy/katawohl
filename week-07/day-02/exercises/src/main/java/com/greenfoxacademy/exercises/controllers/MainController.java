d9d3f8b0474263916abbafc1965ee2624f4d5f6d
Kata Wohlmuth <katawohl@gmail.com>
2020-04-21 15:35:51 +0200

    added HTMLception

--- week-07/day-02/exercises/src/main/java/com/greenfoxacademy/exercises/controllers/MainController.java
+++ week-07/day-02/exercises/src/main/java/com/greenfoxacademy/exercises/controllers/MainController.java
@@ -4,6 +4,7 @@ import com.greenfoxacademy.exercises.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -13,5 +14,12 @@ public class MainController {
     BankAccount account = new BankAccount("Simba", 2000, "lion");
     model.addAttribute("simba", account);
     return "index";
+  }
+
+  @GetMapping("/htmlception")
+  public String displayText(Model model){
+    String stringToDisplay = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+    model.addAttribute("text", stringToDisplay);
+    return "htmlception";
   }
 }
