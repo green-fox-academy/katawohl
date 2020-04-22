bae091952044539bd8b96b7a590c658d78e1eb09
Kata Wohlmuth <katawohl@gmail.com>
2020-04-22 16:39:53 +0200

    added todoApp.sql

---
+++ week-07/day-03/todoApp.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: todoApp
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doingList`
+--
+
+DROP TABLE IF EXISTS `doingList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doingList` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(200) NOT NULL,
+  `date_added` date NOT NULL,
+  `date_due` date DEFAULT NULL,
+  `priority` enum('low','medium','high') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doingList`
+--
+
+LOCK TABLES `doingList` WRITE;
+/*!40000 ALTER TABLE `doingList` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doingList` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doneList`
+--
+
+DROP TABLE IF EXISTS `doneList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doneList` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(200) NOT NULL,
+  `date_added` date NOT NULL,
+  `date_due` date DEFAULT NULL,
+  `date_completion` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doneList`
+--
+
+LOCK TABLES `doneList` WRITE;
+/*!40000 ALTER TABLE `doneList` DISABLE KEYS */;
+INSERT INTO `doneList` VALUES (1,'Go outside','2020-04-22','2020-04-22','2020-04-22');
+/*!40000 ALTER TABLE `doneList` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `todoList`
+--
+
+DROP TABLE IF EXISTS `todoList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `todoList` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(200) NOT NULL,
+  `date_added` date NOT NULL,
+  `date_due` date DEFAULT NULL,
+  `priority` enum('low','medium','high') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todoList`
+--
+
+LOCK TABLES `todoList` WRITE;
+/*!40000 ALTER TABLE `todoList` DISABLE KEYS */;
+INSERT INTO `todoList` VALUES (1,'Learn to type','1999-04-17','2001-09-01','high'),(2,'Learn SQL syntax','2020-04-22',NULL,'high'),(3,'Get milk','2020-04-22',NULL,'low'),(4,'Go outside','2020-04-22','2020-04-22','medium'),(5,'Pet dog','2020-04-22',NULL,'high');
+/*!40000 ALTER TABLE `todoList` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-22 16:36:44
