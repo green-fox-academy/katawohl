fb06ca4aa1d37125bfd2d8692bca96dc710486aa
Kata Wohlmuth <katawohl@gmail.com>
2020-04-24 13:08:01 +0200

    added login page (name does not display)

--- week-07/day-05/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-05/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,13 +1,30 @@
 package com.greenfoxacademy.foxclub.controllers;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-  @GetMapping("/")
-  public String showMain(){
+  @GetMapping("/{name}")
+  public String showMain(@RequestParam String name, Model model) {
+    model.addAttribute("foxName", name);
     return "index";
+  }
+
+  @GetMapping("/login")
+  public String showLogin() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String enterName(@RequestParam String name, Model model) {
+    String redirect = "redirect:/?name=" + name;
+    return redirect;
   }
 }
