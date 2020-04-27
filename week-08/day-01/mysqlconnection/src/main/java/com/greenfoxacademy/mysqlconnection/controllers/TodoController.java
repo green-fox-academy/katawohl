44e0ab23b5559390676b28c43d1ec9937d25239f
Kata Wohlmuth <katawohl@gmail.com>
2020-04-27 17:00:06 +0200

    added Listing Todos exercise

--- week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/controllers/TodoController.java
+++ week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/controllers/TodoController.java
@@ -5,11 +5,13 @@ import com.greenfoxacademy.mysqlconnection.repository.TodoRepository;
 import com.greenfoxacademy.mysqlconnection.services.TodoService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -24,10 +26,23 @@ public class TodoController {
   }
 
   @GetMapping( {"/", "/list"})
-  public String list(Model model) {
-    model.addAttribute("todos", todoService.getTodos());
+  public String list(@RequestParam(value = "isDone", required = false) boolean isDone, Model model) {
+    List<Todo> todos = todoService.getTodos().stream()
+        .filter(Todo::isDone)
+        .collect(Collectors.toList());
+
+    List<Todo> todoList;
+
+    if (isDone){
+      todoList = todos;
+    } else {
+      todoList = todoService.getTodos();
+    }
+
+    model.addAttribute("todos", todoList);
     model.addAttribute("todo", new Todo());
     return "todolist";
   }
+
 }
 
--- week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/models/Todo.java
+++ week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/models/Todo.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "todo_list")
+@Table(name = "todos")
 public class Todo {
 
   @Id
