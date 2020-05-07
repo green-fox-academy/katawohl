36e911431cb78841d2d6e581d944d394d265a59b
katawohl <katawohl@gmail.com>
2020-05-07 17:11:29 +0200

    added null object handling of assignee with thymeleaf

--- week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/controllers/AssigneeController.java
+++ week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/controllers/AssigneeController.java
@@ -16,48 +16,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/todo/assignee")
 public class AssigneeController {
-  private final TodoServiceImpl todoService;
-  private final AssigneeServiceImpl assigneeService;
+    private final TodoServiceImpl todoService;
+    private final AssigneeServiceImpl assigneeService;
 
-  @Autowired
-  public AssigneeController(TodoServiceImpl todoService, AssigneeServiceImpl assigneeService) {
-    this.todoService = todoService;
-    this.assigneeService = assigneeService;
-  }
+    @Autowired
+    public AssigneeController(TodoServiceImpl todoService, AssigneeServiceImpl assigneeService) {
+        this.todoService = todoService;
+        this.assigneeService = assigneeService;
+    }
 
-  @GetMapping("/add")
-  public String showAddAssigneePage() {
-    return "addassignee";
-  }
+    @GetMapping("/add")
+    public String showAddAssigneePage() {
+        return "addassignee";
+    }
 
-  @PostMapping("/add")
-  public String addNewAssignee(@ModelAttribute Assignee assignee) {
-    assigneeService.addAssignee(assignee);
-    return "redirect:/todo/assignees";
-  }
+    @PostMapping("/add")
+    public String addNewAssignee(@ModelAttribute Assignee assignee) {
+        assigneeService.addAssignee(assignee);
+        return "redirect:/todo/assignees";
+    }
 
-  @GetMapping("/{id}/delete")
-  public String deleteTodo(@PathVariable(name = "id") Long id,
-                           @ModelAttribute Assignee assignee) {
-    assigneeService.deleteById(id);
-    return "redirect:/todo/assignees";
-  }
+    @GetMapping("/{id}/delete")
+    public String deleteTodo(@PathVariable(name = "id") Long id) {
+        assigneeService.deleteById(id);
+        return "redirect:/todo/assignees";
+    }
 
-  @GetMapping("/{id}/edit")
-  public String showEditPage(@PathVariable(name = "id") long id, Model model) {
-    model.addAttribute("assignee", assigneeService.findById(id));
-    return "editassignee";
-  }
+    @GetMapping("/{id}/edit")
+    public String showEditPage(@PathVariable(name = "id") long id, Model model) {
+        Assignee assignee = assigneeService.findById(id);
 
-  @PostMapping("/{id}/edit")
-  public String editTodo(@ModelAttribute Assignee assignee) {
-    assigneeService.addAssignee(assignee);
-    return "redirect:/todo/assignees";
-  }
+        model.addAttribute("found", assignee != null);
+        model.addAttribute("assignee", assignee);
+        return "editassignee";
+    }
 
-  @GetMapping("/{id}/todos")
-  public String showAssigneesTodos(@PathVariable(name = "id") long id, Model model){
-    model.addAttribute("todos", assigneeService.findById(id).getTodoList());
-    return "todolist";
-  }
+    @PostMapping("/{id}/edit")
+    public String editTodo(@ModelAttribute Assignee assignee) {
+        assigneeService.addAssignee(assignee);
+        return "redirect:/todo/assignees";
+    }
+
+    @GetMapping("/{id}/todos")
+    public String showAssigneesTodos(@PathVariable(name = "id") long id, Model model) {
+        model.addAttribute("todos", assigneeService.findById(id).getTodoList());
+        return "todolist";
+    }
 }
