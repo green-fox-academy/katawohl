7d64b9c2c3c09f2916ff86cbed40c01aa23489cb
katawohl <katawohl@gmail.com>
2020-05-07 17:42:19 +0200

    started backendAPI exercises

---
+++ week-09/day-01/src/main/java/com/api/demo/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.api.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String renderIndex(){
+        return "index";
+    }
+}
