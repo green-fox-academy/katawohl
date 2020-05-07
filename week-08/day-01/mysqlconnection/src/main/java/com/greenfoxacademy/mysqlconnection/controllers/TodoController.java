3fa6a9340b54cb27fbf53983ab47f6c6f23fa718
katawohl <katawohl@gmail.com>
2020-05-07 17:08:25 +0200

    reformatted TodoController code

--- week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/controllers/TodoController.java
+++ week-08/day-01/mysqlconnection/src/main/java/com/greenfoxacademy/mysqlconnection/controllers/TodoController.java
@@ -18,65 +18,65 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/todo")
 public class TodoController {
 
-  private final TodoServiceImpl todoService;
-  private final AssigneeServiceImpl assigneeService;
+    private final TodoServiceImpl todoService;
+    private final AssigneeServiceImpl assigneeService;
 
-  @Autowired
-  public TodoController(TodoServiceImpl todoService, AssigneeServiceImpl assigneeService) {
-    this.todoService = todoService;
-    this.assigneeService = assigneeService;
-  }
+    @Autowired
+    public TodoController(TodoServiceImpl todoService, AssigneeServiceImpl assigneeService) {
+        this.todoService = todoService;
+        this.assigneeService = assigneeService;
+    }
 
-  @GetMapping( {"/", "/list"})
-  public String list(@RequestParam(value = "isDone", required = false) Boolean isDone,
-                     Model model) {
-    model.addAttribute("todos", todoService.listDoneTodos(isDone));
-    model.addAttribute("todo", new Todo());
-    return "todolist";
-  }
+    @GetMapping({"/", "/list"})
+    public String list(@RequestParam(value = "isDone", required = false) Boolean isDone,
+                       Model model) {
+        model.addAttribute("todos", todoService.listDoneTodos(isDone));
+        model.addAttribute("todo", new Todo());
+        return "todolist";
+    }
 
-  @GetMapping("/assignees")
-  public String showAddAssigneePage(Model model) {
-    model.addAttribute("assignees", assigneeService.returnAllAssignee());
-    return "listofass";
-  }
+    @GetMapping("/assignees")
+    public String showAddAssigneePage(Model model) {
+        model.addAttribute("assignees", assigneeService.returnAllAssignee());
+        return "listofass";
+    }
 
-  @GetMapping("/add")
-  public String showAddPage() {
-    return "addtodopage";
-  }
+    @GetMapping("/add")
+    public String showAddPage() {
+        return "addtodopage";
+    }
 
-  @PostMapping("/add")
-  public String addNewTodo(@ModelAttribute Todo todo) {
-    todoService.addTodo(todo);
-    return "redirect:/todo/list";
-  }
+    @PostMapping("/add")
+    public String addNewTodo(@ModelAttribute Todo todo) {
+        todoService.addTodo(todo);
+        return "redirect:/todo/list";
+    }
 
-  @GetMapping("/{id}/delete")
-  public String deleteTodo(@PathVariable(name = "id") long id) {
-    todoService.deleteById(id);
-    return "redirect:/todo/list";
-  }
+    @GetMapping("/{id}/delete")
+    public String deleteTodo(@PathVariable(name = "id") long id) {
+        todoService.deleteById(id);
+        return "redirect:/todo/list";
+    }
 
-  @GetMapping("/{id}/edit")
-  public String showEditPage(@PathVariable(name = "id") long id, Model model) {
-    Todo todo =todoService.findById(id);
-    model.addAttribute("found", todo != null);
-    model.addAttribute("todo", todo);
-    model.addAttribute("assignees", assigneeService.returnAllAssignee());
-    return "edittodopage";
-  }
+    @GetMapping("/{id}/edit")
+    public String showEditPage(@PathVariable(name = "id") long id, Model model) {
+        Todo todo = todoService.findById(id);
+        model.addAttribute("found", todo != null);
+        model.addAttribute("todo", todo);
+        model.addAttribute("assignees", assigneeService.returnAllAssignee());
+        return "edittodopage";
+    }
 
-  @PostMapping("/{id}/edit")
-  public String editTodo(@ModelAttribute Todo todo, long assigneeId) {
-    todoService.addAssigneeToTodo(assigneeService.findById(assigneeId), todo);
-    return "redirect:/todo/list";
-  }
+    @PostMapping("/{id}/edit")
+    public String editTodo(@ModelAttribute Todo todo, long assigneeId) {
+        todoService.addAssigneeToTodo(assigneeService.findById(assigneeId), todo);
+        return "redirect:/todo/list";
+    }
 
-  @GetMapping("/search")
-  public String searchForTodo(@RequestParam(name = "searchTitle") String title, Model model){
-    model.addAttribute("todos", todoService.findByTitle(title));
-    return "searchresults";
-  }
+    @GetMapping("/search")
+    public String searchForTodo(@RequestParam(name = "searchTitle") String title, Model model) {
+        model.addAttribute("todos", todoService.findByTitle(title));
+        return "searchresults";
+    }
 
 }
