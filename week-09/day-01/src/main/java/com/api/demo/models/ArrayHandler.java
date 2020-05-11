ffed5cf9405267885e2ba419e5ab8fb1efb8ff06
katawohl <katawohl@gmail.com>
2020-05-11 15:23:52 +0200

    deleted unused methods

--- week-09/day-01/src/main/java/com/api/demo/models/ArrayHandler.java
+++ week-09/day-01/src/main/java/com/api/demo/models/ArrayHandler.java
@@ -15,29 +15,4 @@ public class ArrayHandler {
         return numbers;
     }
 
-    public Integer sumNumbers(){
-        Integer sumOfNums = 0;
-        for (int i = 0; i < numbers.size(); i++) {
-            sumOfNums += numbers.get(i);
-        }
-        return sumOfNums;
-    }
-
-    public Integer multiply(){
-        Integer multOfNums = 1;
-        for (int i = 0; i < this.numbers.size(); i++) {
-            multOfNums *= numbers.get(i);
-        }
-        return multOfNums;
-    }
-
-    public List<Integer> doubleList(){
-        List<Integer> doubledList = new ArrayList<>();
-        for (Integer number : this.numbers) {
-            doubledList.add(number*2);
-        }
-        return doubledList;
-    }
-
-
 }
