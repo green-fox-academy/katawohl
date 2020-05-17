627d39340d86a65be4f933421229229c49c109d0
katawohl <katawohl@gmail.com>
2020-05-17 11:08:21 +0200

    set up aliaser project

---
+++ week-10/day-05/aliaser/src/main/java/com/greenfox/aliaser/AliaserApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.aliaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliaserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliaserApplication.class, args);
+    }
+
+}
