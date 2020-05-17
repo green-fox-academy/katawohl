6a653af8a6256e920a229cabb7c2385f91d43700
katawohl <katawohl@gmail.com>
2020-05-17 10:35:02 +0200

    deleted petshelter to pull from remote

--- week-10/Day03/petshelter/src/main/java/com/greenfoxacademy/petshelter/services/PetService.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy.petshelter.services;
-
-import com.greenfoxacademy.petshelter.models.Pet;
-
-import java.util.List;
-
-public interface PetService {
-    List<Pet> listAllPets();
-}
