c45ab622047bce371e7d44b20db9248a0f2d5a5e
katawohl <katawohl@gmail.com>
2020-05-19 10:47:22 +0200

    added one service test

---
+++ week-10/day-05/aliaser/src/test/java/com/greenfox/aliaser/services/LinkServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.greenfox.aliaser.services;
+
+import com.greenfox.aliaser.models.Link;
+import com.greenfox.aliaser.repositories.LinkRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkServiceImplTest {
+
+    @Test
+    void findByAliasShouldReturnCorrectLink() {
+        LinkRepository repository = Mockito.mock(LinkRepository.class);
+
+        LinkServiceImpl service = new LinkServiceImpl(repository);
+
+        Link link = new Link();
+        Link link1 = new Link();
+
+        link.setAlias("test");
+        link1.setAlias("test1");
+
+        Mockito.when(repository.findByAlias("test")).thenReturn(link);
+
+        Link testLink = service.findByAlias("test");
+
+        Assert.assertEquals(testLink, link);
+    }
+}
