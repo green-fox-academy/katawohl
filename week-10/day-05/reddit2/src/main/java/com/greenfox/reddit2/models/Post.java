320b0f952d62405907c04aeda69ddd8f8d424091
katawohl <katawohl@gmail.com>
2020-05-20 08:54:28 +0200

    added JPA annot to Post

--- week-10/day-05/reddit2/src/main/java/com/greenfox/reddit2/models/Post.java
+++ week-10/day-05/reddit2/src/main/java/com/greenfox/reddit2/models/Post.java
@@ -1,11 +1,29 @@
 package com.greenfox.reddit2.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String url;
     private String title;
     private int likeCount;
 
     public Post() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getUrl() {
