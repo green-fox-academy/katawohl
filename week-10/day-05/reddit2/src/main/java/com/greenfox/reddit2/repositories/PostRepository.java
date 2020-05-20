64be9efc0768013fa5b5faed00d898042cd8c060
katawohl <katawohl@gmail.com>
2020-05-20 08:54:42 +0200

    created Post repository

---
+++ week-10/day-05/reddit2/src/main/java/com/greenfox/reddit2/repositories/PostRepository.java
@@ -0,0 +1,7 @@
+package com.greenfox.reddit2.repositories;
+
+import com.greenfox.reddit2.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
