f4802f2e7bfa18edf2bee57c96297c5502af7d82
katawohl <katawohl@gmail.com>
2020-05-20 08:43:43 +0200

    project created

---
+++ week-10/day-05/reddit2/src/test/java/com/greenfox/reddit2/Reddit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.reddit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reddit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
