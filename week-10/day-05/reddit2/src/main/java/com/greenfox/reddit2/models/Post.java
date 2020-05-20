916dad7acd5e036e75662229ca5d9d1bfbc6dc04
katawohl <katawohl@gmail.com>
2020-05-20 08:51:48 +0200

    added Post class

---
+++ week-10/day-05/reddit2/src/main/java/com/greenfox/reddit2/models/Post.java
@@ -0,0 +1,34 @@
+package com.greenfox.reddit2.models;
+
+public class Post {
+    private String url;
+    private String title;
+    private int likeCount;
+
+    public Post() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+}
