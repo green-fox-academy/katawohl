cea99f2715e8475ab7ca274654c2cbf479c800e0
katawohl <katawohl@gmail.com>
2020-05-20 11:48:48 +0200

    added extra getters and setters to Post model

--- week-10/day-05/reddit2/src/main/java/com/greenfox/reddit2/models/Post.java
+++ week-10/day-05/reddit2/src/main/java/com/greenfox/reddit2/models/Post.java
@@ -62,4 +62,12 @@ public class Post {
     public void setDate(Date date) {
         this.date = date;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
